4d7130728a1517ad992ab4344f9225760d9955d9
Viktor Shishkalov <viktorshish@gmail.com>
2023-03-05 00:26:02 +0300

    Add ephem planet

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -13,7 +13,9 @@
 
 """
 import logging
+import datetime
 import settings
+import ephem
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -23,19 +25,55 @@ logger = logging.getLogger(__name__)
 
 def greet_user(update, context):
     logger.info('Вызван /Start')
-    update.message.reply_text('Здравствуй, пользователь!')
+    update.message.reply_text('Здравствуй, пользователь! Введите команду: /planet [Название планеты на английском языке]')
 
-def talk_to_me(update, context):
-    text = update.message.text
-    logger.info(f'сообщение ЭХО: {text}')
-    update.message.reply_text(text)
+def planet_in_the_constellation(update, context):
+    planet = update.message.text.split()[1].lower()
+    if planet == 'mercury':
+        mercury = ephem.Mercury(datetime.datetime.now())
+        constellation = ephem.constellation(mercury)
+    elif planet == 'venus':
+        venus = ephem.Venus(datetime.datetime.now())
+        constellation = ephem.constellation(venus)
+    elif planet == 'jupiter':
+        jupiter = ephem.Jupiter(datetime.datetime.now())
+        constellation = ephem.constellation(jupiter)
+    elif planet == 'saturn':
+        saturn = ephem.Saturn(datetime.datetime.now())
+        constellation = ephem.constellation(saturn)
+    elif planet == 'uranus':
+        uranus = ephem.Uranus(datetime.datetime.now())
+        constellation = ephem.constellation(uranus)
+    elif planet == 'neptune':
+        neptune = ephem.Neptune(datetime.datetime.now())
+        constellation = ephem.constellation(neptune)
+
+
+    else:
+        print('Ничего не выбрано')
+
+    print(f'На текущее время: {datetime.datetime.now()} - планета {planet.capitalize()} находдится в созведии {constellation}')
+
+    # print('Ничего не выбрано')
+# def talk_to_me(update, context):
+#     text = update.message.text.split()[1]
+#     if text =='mars':
+#         mars = ephem.Mars(datetime.today())
+#         constellation = ephem.constellation(mars)
+#     else:
+#         logger.info(f'Ничего не выбрано')
+#         update.message.reply_text('Ничего не выбрано')
+#     logger.info(f'Выбрана планета: {text}')
+#     update.message.reply_text(f'Выбрана планета: {text} - Находится в созвездии {constellation}')
 
 def main():
     mybot = Updater(settings.API_KEY, use_context=True)
 
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler('planet', planet_in_the_constellation))
+    dp.add_handler(MessageHandler(Filters.text, planet_in_the_constellation))
+
 
     logger.info('Бот запущен')
     mybot.start_polling()
